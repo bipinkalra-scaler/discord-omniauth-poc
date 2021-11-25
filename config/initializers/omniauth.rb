51d967699da4c52fb269c2f626507ad58e9b61cc
bipinkalra-scaler <bipin.kalra@scaler.com>
2021-11-26 00:51:33 +0530

    redirect URL fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :discord, '913488322402332682', 'RVn6f0bOuXIVQKnKFupFtEQI8H0EJBRd', scope: 'email identify', callback_url: 'https://someurl.com/users/auth/discord/callback'
+  provider :discord, '913488322402332682', 'RVn6f0bOuXIVQKnKFupFtEQI8H0EJBRd', scope: 'email identify', callback_url: 'http://localhost:3000/users/auth/discord/callback'
 end
